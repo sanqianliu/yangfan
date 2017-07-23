7af55bbb00b088feec03c50b4c9a7379ab927aa7
yangfan <976404963@qq.com>
2017-07-23 22:51:55 +0800

    修改入口

--- basic/web/index.php
+++ basic/web/index.php
@@ -1,4 +1,6 @@
 <?php
+
+echo 5373;
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
